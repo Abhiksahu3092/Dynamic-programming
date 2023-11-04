8d34697f6c913e7e5bd2f481589dcd861c463f5f
Abhik Sahu <abhiksahu2003@gmail.com>
2023-11-04 17:36:53 +0530

    nothing

--- 2.cpp
+++ 2.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 
 int arr[]={10,20,50,1,100};
-int r=sizeof(arr)/sizeof(arr[0]);
+int r=sizeof(arr)/sizeof(arr[0]);//value of n is 4
 int n=r-1;
 
 int matrix[4][4];
 int s[4][4];
 int mcm(){
-    for(int i=1;i<=n;i++){
-        matrix[i][i]=0;
-       
-    }
-    return matrix[1][n];
+    
 }
 
 int main(){
